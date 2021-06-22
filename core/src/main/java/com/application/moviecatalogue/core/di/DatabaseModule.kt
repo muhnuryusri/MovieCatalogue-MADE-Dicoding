b254c29367dcef4881088f7032cd517e0e3b3919
muhnuryusri <naoruyuzuri@gmail.com>
2021-06-23 00:37:04 +0800

    encrypting database

--- core/src/main/java/com/application/moviecatalogue/core/di/DatabaseModule.kt
+++ core/src/main/java/com/application/moviecatalogue/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,10 +19,15 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): CatalogueDatabase = Room.databaseBuilder(
-            context,
-            CatalogueDatabase::class.java, "favorites.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): CatalogueDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("muhnuryusri".toCharArray())
+        val factory = SupportFactory(passphrase)
+
+        return Room.databaseBuilder(context, CatalogueDatabase::class.java, "Favorite.db")
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideCatalogueDao(database: CatalogueDatabase): CatalogueDao =
